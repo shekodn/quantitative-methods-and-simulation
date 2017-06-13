dd35f6cab3a6d7515ee5704e16036ab0bbcc3cf9
Daniel Gonzalez <dano.gonzalez0@gmail.com>
2017-06-13 15:47:20 -0500

    Grafica de booking vs Edad y Mes

--- Lectura.py
+++ Lectura.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-
-
 # Set figure aesthetics
 sns.set_style("white", {'ytick.major.size': 10.0})
 sns.set_context("poster", font_scale=1.1)
@@ -14,16 +12,10 @@ sns.set_context("poster", font_scale=1.1)
 # Load the data into DataFrames
 train_users = pd.read_csv('train_users_2.csv')
 test_users = pd.read_csv('test_users.csv')
-
-print("We have", train_users.shape[0], "users in the training set and",
-      test_users.shape[0], "in the test set.")
-print("In total we have", train_users.shape[0] + test_users.shape[0], "users.")
+sessions_users = pd.read_csv('sessions.csv')
 
 # Merge train and test users
 users = pd.concat((train_users, test_users), axis=0, ignore_index=True)
-
-# Remove ID's since now we are not interested in making predictions
-users.drop('id',axis=1, inplace=True)
 
 users.head()
 
@@ -32,15 +24,7 @@ users.gender.replace('-unknown-', np.nan, inplace=True)
 users_nan = (users.isnull().sum() / users.shape[0]) * 100
 users_nan[users_nan > 0].drop('country_destination')
 
-print("Just for the sake of curiosity; we have",
-      int((train_users.date_first_booking.isnull().sum() / train_users.shape[0]) * 100),
-      "% of missing values at date_first_booking in the training data")
-
-
 users.age.describe()
-
-print(sum(users.age > 122))
-print(sum(users.age < 18))
 
 users[users.age > 122]['age'].describe()
 
@@ -49,6 +33,7 @@ users[users.age < 18]['age'].describe()
 users.loc[users.age > 95, 'age'] = np.nan
 users.loc[users.age < 13, 'age'] = np.nan
 
+########################################
 categorical_features = [
     'affiliate_channel',
     'affiliate_provider',
@@ -67,106 +52,23 @@ for categorical_feature in categorical_features:
 
 users['date_account_created'] = pd.to_datetime(users['date_account_created'])
 users['date_first_booking'] = pd.to_datetime(users['date_first_booking'])
+#print(users)
 users['date_first_active'] = pd.to_datetime((users.timestamp_first_active // 1000000), format='%Y%m%d')
 
-users.gender.value_counts(dropna=False).plot(kind='bar', color='#FD5C64', rot=0)
-plt.xlabel('Gender')
-sns.despine()
-# plt.show()
 
-women = sum(users['gender'] == 'FEMALE')
-men = sum(users['gender'] == 'MALE')
+cleanUsers = users[users['date_first_booking'].notnull()]
+notCleanUseres = users[users['date_first_booking'].isnull()]
+sessions_users.rename(columns={'user_id':'id'}, inplace=True)
 
-female_destinations = users.loc[users['gender'] == 'FEMALE', 'country_destination'].value_counts() / women * 100
-male_destinations = users.loc[users['gender'] == 'MALE', 'country_destination'].value_counts() / men * 100
-
-# Bar width
-width = 0.4
-
-male_destinations.plot(kind='bar', width=width, color='#4DD3C9', position=0, label='Male', rot=0)
-female_destinations.plot(kind='bar', width=width, color='#FFA35D', position=1, label='Female', rot=0)
-
-plt.legend()
-plt.xlabel('Destination Country')
-plt.ylabel('Percentage')
-
-sns.despine()
-# plt.show()
-
-destination_percentage = users.country_destination.value_counts() / users.shape[0] * 100
-destination_percentage.plot(kind='bar',color='#FD5C64', rot=0)
-# Using seaborn can also be plotted
-# sns.countplot(x="country_destination", data=users, order=list(users.country_destination.value_counts().keys()))
-plt.xlabel('Destination Country')
-plt.ylabel('Percentage')
+#Plot de porcetaje de edades
+plt.xlabel("Edad")
+plt.ylabel("Porcentaje")
+sns.distplot(cleanUsers.age.dropna(), color='#FD5C64')
+plt.show()
 sns.despine()
 
-sns.distplot(users.age.dropna(), color='#FD5C64')
-plt.xlabel('Age')
-sns.despine()
-
-
-age = 45
-
-younger = sum(users.loc[users['age'] < age, 'country_destination'].value_counts())
-older = sum(users.loc[users['age'] > age, 'country_destination'].value_counts())
-
-younger_destinations = users.loc[users['age'] < age, 'country_destination'].value_counts() / younger * 100
-older_destinations = users.loc[users['age'] > age, 'country_destination'].value_counts() / older * 100
-
-younger_destinations.plot(kind='bar', width=width, color='#63EA55', position=0, label='Youngers', rot=0)
-older_destinations.plot(kind='bar', width=width, color='#4DD3C9', position=1, label='Olders', rot=0)
-
-plt.legend()
-plt.xlabel('Destination Country')
-plt.ylabel('Percentage')
-
-sns.despine()
-# plt.show()
-
-sns.set_style("whitegrid", {'axes.edgecolor': '0'})
-sns.set_context("poster", font_scale=1.1)
-users.date_account_created.value_counts().plot(kind='line', linewidth=1.2, color='#FD5C64')
-
-
-users.date_first_active.value_counts().plot(kind='line', linewidth=1.2, color='#FD5C64')
-
-
-users_2013 = users[users['date_first_active'] > pd.to_datetime(20130101, format='%Y%m%d')]
-users_2013 = users_2013[users_2013['date_first_active'] < pd.to_datetime(20140101, format='%Y%m%d')]
-users_2013.date_first_active.value_counts().plot(kind='line', linewidth=2, color='#FD5C64')
-# plt.show()
-
-weekdays = []
-for date in users.date_account_created:
-    weekdays.append(date.weekday())
-weekdays = pd.Series(weekdays)
-
-
-sns.barplot(x = weekdays.value_counts().index, y=weekdays.value_counts().values, order=range(0,7))
-plt.xlabel('Week Day')
-sns.despine()
-
-
-date = pd.to_datetime(20140101, format='%Y%m%d')
-
-before = sum(users.loc[users['date_first_active'] < date, 'country_destination'].value_counts())
-after = sum(users.loc[users['date_first_active'] > date, 'country_destination'].value_counts())
-before_destinations = users.loc[users['date_first_active'] < date,
-                                'country_destination'].value_counts() / before * 100
-after_destinations = users.loc[users['date_first_active'] > date,
-                               'country_destination'].value_counts() / after * 100
-before_destinations.plot(kind='bar', width=width, color='#63EA55', position=0, label='Before 2014', rot=0)
-after_destinations.plot(kind='bar', width=width, color='#4DD3C9', position=1, label='After 2014', rot=0)
-
-plt.legend()
-plt.xlabel('Destination Country')
-plt.ylabel('Percentage')
-
-sns.despine()
-# plt.show()
-
-
-# uncomment only when you want to generate a csv file
-# merged_file = np.asarray(users)
-# np.savetxt("users.csv", merged_file, delimiter=",", fmt='%.5f')
+cleanUsers2013 = cleanUsers[cleanUsers['date_first_booking'] > pd.to_datetime(20130101, format='%Y%m%d')]
+cleanUsers2013 = cleanUseres2013[cleanUseres2013['date_first_booking'] < pd.to_datetime(20140101, format='%Y%m%d')]
+cleanUseres2013.date_first_booking.value_counts().plot(kind='line', linewidth=2, color='#FD5C64')
+plt.show()
+##########################################
