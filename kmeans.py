f13e7ee40f86ec46de64063c8635507d5bcf31c5
Daniel Gonzalez <dano.gonzalez0@gmail.com>
2017-06-22 20:49:38 -0500

    Refactors

--- kmeans.py
+++ kmeans.py
@@ -15,18 +15,6 @@ from dateutil import parser
 
 cleanSessions = pd.read_csv('Datos.csv')
 
-# cont = 0
-# for x in cleanSessions['date_first_booking']:
-#     if x[1] == '/':
-#         cleanSessions['date_first_booking'][cont] = '0' + x
-#     if cleanSessions['date_first_booking'][cont][4] == '/':
-#         test = cleanSessions['date_first_booking'][cont]
-#         cleanSessions['date_first_booking'][cont] = test[:3] + '0' + test[3:]
-#     cont += 1
-#     print(cleanSessions['date_first_booking'][cont])
-
-#
-
 
 cleanSessions['date_first_booking_number'] = cleanSessions['date_first_booking']
 
@@ -39,16 +27,7 @@ for x in cleanSessions['date_first_booking']:
     cont += 1
 
 
-# cont = 0
-# for x in cleanSessions['date_first_booking_number']:
-#     cleanSessions['date_first_booking_number'][cont] = np.datetime64(x).astype('uint64') / 1e6
-#     print(cleanSessions['date_first_booking_number'][cont])
-#     cont += 1
-
-
-
-
-#cleanSessions.to_csv("Datos.csv")
+cleanSessions.to_csv("Datos.csv")
 
 
 KArray = [cleanSessions['bookingtime'], firstBooking, cleanSessions['age']]
