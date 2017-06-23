dfce72bb9f471d97263ddc787bf7b50fdeecf04f
shekodn <sdiazn19@gmail.com>
2017-06-22 19:37:41 -0500

    adds TODO's

--- simulacion.py
+++ simulacion.py
@@ -4,27 +4,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 
+# np.seterr(divide='ignore', invalid='ignore')
+
+
 #Load data set
 data = pd.read_csv('Datos.csv')
 # del data['date_account_created']
 # del data['date_first_active']
 # del data['date_first_booking']
 # del data['signup_method']
-
-# data = data[['age','timestamp_first_active']]
-data = data['age']
+#
 
 # print data.head()
+
+
+#TODO
+# data = data[['age','secs_elapsed']]
+data = data['age']
+
+# data = data[data.secs_elapsed != 0]
+#
+#
+# for i in data['secs_elapsed']:
+#   if i == 0:
+#     print 'PUTA'
+
+
+
+
 
 #convert it to numpy arrays
 X=data.values
 
 #Scaling the values
 X = scale(X)
+
+#TODO
 X = X.reshape(-1,1) # for 1 value
 # X = X.reshape(1,-1) # for more than 1 value
 
-pca = PCA(n_components=1)
+pca = PCA(n_components=1) #TODO
 
 pca.fit(X)
 
