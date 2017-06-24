b4051820feb71889eebf5738932362973509019a
Daniel Gonzalez <dano.gonzalez0@gmail.com>
2017-06-23 19:49:36 -0500

    Calculo de K-means

--- kmeans.py
+++ kmeans.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from matplotlib.finance import date2num
 from scipy import stats
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from matplotlib import pylab
 from dateutil import parser
 
@@ -18,19 +19,28 @@ cleanSessions = pd.read_csv('Datos.csv')
 
 cleanSessions['date_first_booking_number'] = cleanSessions['date_first_booking']
 
-firstBooking = []
+first_Booking = []
 
 cont = 0
 for x in cleanSessions['date_first_booking']:
-    cleanSessions['date_first_booking_number'][cont] = datetime.strptime(x,'%Y-%m-%d')
-    firstBooking.append(np.datetime64(cleanSessions['date_first_booking_number'][cont]).astype('uint64'))
+    first_Booking.append(float((x[5:7])))
+
+#cleanSessions.to_csv("Datos.csv")
+
+cleanSessions['firstBooking'] = np.asarray(first_Booking)
+#print(cleanSessions['bookingtime'])
+KArray = [cleanSessions['bookingtime'], cleanSessions['firstBooking'], cleanSessions['age']]
+
+#Creation of numpy array
+data = []
+cont = 0
+for x in cleanSessions['bookingtime']:
+    y = cleanSessions['firstBooking'][cont]
+    z = cleanSessions['age'][cont]
+    data.append(np.array([x,y,z]))
     cont += 1
 
-
-cleanSessions.to_csv("Datos.csv")
-
-
-KArray = [cleanSessions['bookingtime'], firstBooking, cleanSessions['age']]
-print("LISTO")
-resultFromKMeans = KMeans(n_clusters = 2).fit(KArray)
-print(resultFromKMeans.labels_)
+resultFromKMeans = KMeans(n_clusters = 2, n_jobs = 2).fit(data)
+pcaResults = PCA(n_components = 3).fit(data)
+print("K-Means results: ", resultFromKMeans.labels_)
+print("PCA resultas: ", pcaResults.explained_variance_)
